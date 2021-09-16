0079c4851507a2e855286afd6e4be18d7f90e019
Eugene Shemetov <emshemetov@gmail.com>
2021-09-16 13:49:25 +0300

    first commit

---
+++ force-app/main/default/lwc/onlinerSuck/onlinerSuck.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class OnlinerSuck extends LightningElement {
+    fastMenu = ['Catalog', 'Catalog', 'Flea market'];
+    fastInfo = ['Weather', 'Dollar rate'];
+}
